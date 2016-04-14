4e45a0c5bfcf65dbc548e8882ac63902c146310b
Nico321 <nico.lindmeyer@web.de>
2016-04-14 15:27:35 +0200

    added design and framerate etc. fixed bug

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import application.model.Car;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
@@ -15,6 +16,7 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 
 public class Controller {
 
@@ -50,13 +52,23 @@ public class Controller {
 	private TextField tfdisplayStart, tfdisplayStop;
 	private Integer displayStart = 0, displayStop = 0;
 
+	@FXML
+	private TextField tfMaxSpeed, tfCarSize;
+	
+	@FXML
+	private TextField tfFramerate;
+	private Integer framerate=1000;
+
 	private boolean pause = false, stop = true;
 
 	private Car street[][];
-	private Double p, p0, dichte, dichteAbs;
+	private Double p, p0, dichte;
+	private Long dichteAbs;
 	private Integer size;
-	private final int CAR_SIZE = 10;
-	private final int MAX_SPEED = 5;
+	private int CAR_SIZE = 10;
+	private int MAX_SPEED = 5;
+	private Canvas canvas;
+	private GraphicsContext gc;
 
 	@FXML
 	private void pause() {
@@ -65,6 +77,20 @@ public class Controller {
 		btnStop.setDisable(false);
 		btnPause.setDisable(true);
 		piSimulating.setVisible(false);
+		
+		tfp.setDisable(true);
+		tfp0.setDisable(true);
+		tfC.setDisable(true);
+		rbDichteAbs.setDisable(true);
+		rbDichteProz.setDisable(true);
+		tfdichte.setDisable(true);
+		tfDichteAbs.setDisable(true);
+		tfsize.setDisable(true);
+		tfdisplayStart.setDisable(false);
+		tfdisplayStop.setDisable(false);
+		tfMaxSpeed.setDisable(true);
+		tfCarSize.setDisable(false);
+		tfFramerate.setDisable(false);
 	}
 
 	@FXML
@@ -74,6 +100,20 @@ public class Controller {
 		btnPause.setDisable(true);
 		btnStop.setDisable(true);
 		piSimulating.setVisible(false);
+		
+		tfp.setDisable(false);
+		tfp0.setDisable(false);
+		tfC.setDisable(false);
+		rbDichteAbs.setDisable(false);
+		rbDichteProz.setDisable(false);
+		tfdichte.setDisable(false);
+		tfDichteAbs.setDisable(false);
+		tfsize.setDisable(false);
+		tfdisplayStart.setDisable(false);
+		tfdisplayStop.setDisable(false);
+		tfMaxSpeed.setDisable(false);
+		tfCarSize.setDisable(false);
+		tfFramerate.setDisable(false);
 	}
 
 	@FXML
@@ -89,9 +129,29 @@ public class Controller {
 
 	@FXML
 	private void simulate() {
+		
+		tfp.setDisable(true);
+		tfp0.setDisable(true);
+		tfC.setDisable(true);
+		rbDichteAbs.setDisable(true);
+		rbDichteProz.setDisable(true);
+		tfdichte.setDisable(true);
+		tfDichteAbs.setDisable(true);
+		tfsize.setDisable(true);
+		tfdisplayStart.setDisable(true);
+		tfdisplayStop.setDisable(true);
+		tfMaxSpeed.setDisable(true);
+		tfCarSize.setDisable(true);
+		tfFramerate.setDisable(true);
+		
 		pause = false;
+		streetpane.getChildren().removeAll(streetpane.getChildren());
+		canvas = null;
 		displayStart = Integer.parseInt(tfdisplayStart.getText());
 		displayStop = Integer.parseInt(tfdisplayStop.getText());
+		if(!tfFramerate.getText().equals(""))
+			framerate = Integer.parseInt(tfFramerate.getText());
+		CAR_SIZE = Integer.parseInt(tfCarSize.getText());
 
 		btnPlay.setDisable(true);
 		btnPause.setDisable(false);
@@ -104,10 +164,12 @@ public class Controller {
 			p = Double.parseDouble(tfp.getText());
 			p0 = Double.parseDouble(tfp0.getText());
 			c = Double.parseDouble(tfC.getText());
-			if(!tfdichte.getText().equals(""))
+			if (!tfdichte.getText().equals(""))
 				dichte = Double.parseDouble(tfdichte.getText());
-			if(!tfDichteAbs.getText().equals(""))
-				dichteAbs = Double.parseDouble(tfDichteAbs.getText());
+			if (!tfDichteAbs.getText().equals(""))
+				dichteAbs = Long.parseLong(tfDichteAbs.getText());
+			
+			MAX_SPEED = Integer.parseInt(tfMaxSpeed.getText());
 			size = Integer.parseInt(tfsize.getText());
 			initSimulation();
 			stop = false;
@@ -117,12 +179,13 @@ public class Controller {
 			public void run() {
 				while (!pause && !stop) {
 					numSteps++;
+					changeTracks();
 					accelerateCars();
 					breakCars();
 					dawdleCars();
 					moveCars();
 
-					if (numSteps % 1000 == 0) {
+					if (numSteps % framerate == 0) {
 						Platform.runLater(new Runnable() {
 							public void run() {
 								draw();
@@ -137,31 +200,55 @@ public class Controller {
 
 	private void initSimulation() {
 		street = new Car[2][size];
-		
-		if(rbDichteProz.isSelected())
+
+		if (rbDichteProz.isSelected())
 			initDichteProz();
 		else
 			initDichteAbs();
 	}
 
-	private void initDichteAbs(){
+	private void initDichteAbs() {
 		Random random = new Random();
-		while(dichteAbs > 0){
+		if (dichteAbs == null)
+			dichteAbs = 0L;
+		while (dichteAbs > 0) {
 			int t = random.nextInt((1 - 0) + 1);
 			int pos = random.nextInt(size);
-			if(street[t][pos] == null){
-				street[t][pos] = new Car(random.nextInt((5 - 1) + 1) + 1);
+			if (street[t][pos] == null) {
+				street[t][pos] = new Car(random.nextInt((MAX_SPEED - 1) + 1) + 1);
 				dichteAbs--;
 			}
 		}
 	}
-	
+
 	private void initDichteProz() {
 		Random random = new Random();
 		for (int t = 0; t < street.length; t++) {
 			for (int i = 0; i < size; i++) {
 				if (random.nextFloat() < dichte) {
-					street[t][i] = new Car(random.nextInt((5 - 1) + 1) + 1);
+					street[t][i] = new Car(random.nextInt((MAX_SPEED - 1) + 1) + 1);
+				}
+			}
+		}
+	}
+
+	private void changeTracks() {
+		for (int t = 0; t < street.length; t++) {
+			for (int i = 0; i < size; i++) {
+				if (street[t][i] != null && street[t][i].getSpeed() > 0) {
+					for (int j = 1; j <= street[t][i].getSpeed()+1; j++) {
+						int check = i + j;
+						if (i + j >= size)
+							check -= size;
+						if (street[t][check] != null) {
+							if (checkTrackChange(i, t, 1 - t)) {
+								street[1 - t][i] = street[t][i];
+								street[t][i] = null;
+							}
+
+							break;
+						}
+					}
 				}
 			}
 		}
@@ -187,12 +274,7 @@ public class Controller {
 						if (i + j >= size)
 							check -= size;
 						if (street[t][check] != null) {
-							if(!checkTrackChange(i,t, 1-t)){
-								street[t][i].setSpeed(j - 1);
-							}else{
-								street[1-t][i] = street[t][i];
-								street[t][i] = null;
-							}
+							street[t][i].setSpeed(j - 1);
 							break;
 						}
 					}
@@ -200,39 +282,38 @@ public class Controller {
 			}
 		}
 	}
-	
-	private boolean checkTrackChange(int pos, int currentTrack, int targetTrack){
-		
-		//check behind cars
-		for(int i=1;i<=5;i++){
-			int check = pos-i;
-			if(check < 0)
+
+	private boolean checkTrackChange(int pos, int currentTrack, int targetTrack) {
+
+		// check behind cars
+		for (int i = 1; i <= 5; i++) {
+			int check = pos - i;
+			if (check < 0)
 				check += size;
-			if(street[targetTrack][check] != null){
+			if (street[targetTrack][check] != null) {
 				return false;
 			}
 		}
-		
-		//check neighbour car
-		if(street[targetTrack][pos]!=null)
+
+		// check neighbour car
+		if (street[targetTrack][pos] != null)
 			return false;
-		
-		//check previous cars
-		for(int i=1;i<=street[currentTrack][pos].getSpeed()+1;i++){
-			int check = pos+i;
-			if(check >= size)
-				check -=size;
-			if(street[targetTrack][check] != null){
+
+		// check previous cars
+		for (int i = 1; i <= street[currentTrack][pos].getSpeed() + 1; i++) {
+			int check = pos + i;
+			if (check >= size)
+				check -= size;
+			if (street[targetTrack][check] != null) {
 				return false;
 			}
 		}
-		
-		//Check track change possibility
+
+		// Check track change possibility
 		Random random = new Random();
 		if (random.nextFloat() < c) {
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
@@ -284,12 +365,36 @@ public class Controller {
 	}
 
 	private void draw() {
-		Canvas canvas = new Canvas(streetpane.getWidth(), streetpane.getHeight());
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		streetpane.getChildren().add(canvas);
-		int trackOffset = 0;
+		if (canvas == null) {
+			canvas = new Canvas(streetpane.getWidth(), streetpane.getHeight());
+			gc = canvas.getGraphicsContext2D();
+			streetpane.getChildren().add(canvas);
+		}
+		gc.setTextAlign(TextAlignment.CENTER);
+		gc.setTextBaseline(VPos.CENTER);
+		gc.clearRect(0, 0, streetpane.getWidth(), streetpane.getHeight());
+
+		for (int i = 0; i <= MAX_SPEED + 1; i++) {
+			if (i == 0)
+				gc.setFill(Color.WHITE);
+			else if (i > MAX_SPEED)
+				gc.setFill(Color.BLACK);
+			else
+				gc.setFill(
+						Color.hsb((Color.GREEN.getHue() + (Color.RED.getHue() - Color.GREEN.getHue()) * i / MAX_SPEED),
+								1.0, 1.0));
+			gc.fillPolygon(new double[] { (i * 25) + 10, (i * 25) + 20, (i * 25) + 20, (i * 25) + 10 },
+					new double[] { 0, 0, 10, 10 }, 4);
+			gc.setFill(Color.BLACK);
+
+			if (i > MAX_SPEED)
+				gc.fillText("no car", (i * 25) + 40, 5);
+			else
+				gc.fillText(i + "", (i * 25) + 26, 5);
+		}
+
+		int trackOffset = 20;
 		for (int t = street.length - 1; t >= 0; t--) {
-			trackOffset += CAR_SIZE;
 			int offset = 0;
 			for (int i = displayStart; i <= displayStop; i++) {
 
@@ -297,38 +402,20 @@ public class Controller {
 				if (c == null) {
 					gc.setFill(Color.BLACK);
 				} else {
-					switch (c.getSpeed()) {
-					case 0:
+					if (c.getSpeed() == 0)
 						gc.setFill(Color.WHITE);
-						break;
-					case 1:
-						gc.setFill(Color.GREEN);
-						break;
-					case 2:
-						gc.setFill(Color.YELLOWGREEN);
-						break;
-					case 3:
-						gc.setFill(Color.YELLOW);
-						break;
-					case 4:
-						gc.setFill(Color.ORANGE);
-						break;
-					case 5:
-						gc.setFill(Color.RED);
-						break;
-
-					default:
-						System.out.println(c.getSpeed());
-						gc.setFill(Color.PINK);
-						break;
-					}
+					else
+						gc.setFill(Color.hsb(
+								(Color.GREEN.getHue()
+										+ (Color.RED.getHue() - Color.GREEN.getHue()) * c.getSpeed() / MAX_SPEED),
+								1.0, 1.0));
 				}
 
 				gc.fillPolygon(new double[] { offset, offset + CAR_SIZE, offset + CAR_SIZE, offset }, new double[] {
 						0 + trackOffset, 0 + trackOffset, CAR_SIZE + trackOffset, CAR_SIZE + trackOffset }, 4);
 				offset += CAR_SIZE;
 			}
-
+			trackOffset += CAR_SIZE;
 		}
 
 		gc.setFill(Color.BLUE);
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -9,34 +9,30 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 	private Stage primaryStage;
 	private AnchorPane rootLayout;
-	
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Traffic Simulation");
+		this.primaryStage.setTitle("Traffic Simulation");
 		try {
-            // Load root layout from fxml file.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("view/SimulationOverview.fxml"));
-            rootLayout = (AnchorPane) loader.load();
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/SimulationOverview.fxml"));
+			rootLayout = (AnchorPane) loader.load();
 
-            // Show the scene containing the root layout.
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
-            primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("stau.png")));
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("stau.png")));
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
